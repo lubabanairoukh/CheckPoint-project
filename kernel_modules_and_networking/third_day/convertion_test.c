57e08c88a49da9b1b4c7acf3e040849d74dbb068
YanivGabay <yanivga@edu.hac.ac.il>
2024-09-17 15:25:29 +0300

    ex4, with structs of ip, the range of comparing is too big

--- kernel_modules_and_networking/third_day/convertion_test.c
+++ kernel_modules_and_networking/third_day/convertion_test.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <arpa/inet.h> // Header where htonl() and ntohl() are defined
 
+#include <linux/types.h>
 int main() {
     uint32_t host_order_number = 0xAABBCCDD;
     uint32_t network_order_number;
 
+    __be32 addr = in_aton("1.1.1.1");
+    __be32 net_mask = htonl(~((1 << (32 - 20))-1));
+
+    __be32 network_addr = addr & net_mask;
+
+    printf("Network Address: %pI4\n", &network_addr);
+    printf("Network Mask: %pI4\n", &net_mask);
+    printf("Address: %pI4\n", &addr);
     // Convert from host order to network order
     network_order_number = htonl(host_order_number);
     printf("Host Order: 0x%X, Network Order: 0x%X\n", host_order_number, network_order_number);
--- kernel_modules_and_networking/third_day_4ex/netfilter_module.c
+++ kernel_modules_and_networking/third_day_4ex/netfilter_module.c
@@ -14,39 +14,45 @@ MODULE_AUTHOR("Student");
 MODULE_DESCRIPTION("Netfilter Kernel Module Example");
 // Hook function definition
 
+
+struct cidr_block{
+    char* ip_str;
+    int prefix;
+    __be32 first_addr;
+    __be32 last_addr;
+};
+static struct cidr_block cidr_blocks[] = {
+    {"1.10.16.0" ,20,0,0},
+    {"1.19.0.0" , 16,0,0}
+};
+static int cidr_blocks_size = sizeof(cidr_blocks)/sizeof(struct cidr_block);
 static unsigned int packet_hook(void *priv,
                                 struct sk_buff *skb,
                                 const struct nf_hook_state *state)
 {
 
     struct iphdr *ip_header = ip_hdr(skb);
-
-    if (ip_header)
+    if(ip_header->protocol != IPPROTO_TCP)
     {
+        return NF_ACCEPT;
+    }
+    __be32 src_ip = ip_header->saddr;
 
-        char *ip = "8.8.8.8";
-        __be32 block_ip = in_aton(ip);
-        if (ip_header->saddr == block_ip)
-        {
-            __be32 addr = in_aton("1.1.1.1");
-            __be32 net_mask = htonl(~((1 << (32 - 20)) - 1));
-            
-            __be32 first_addr = addr & net_mask;
-            __be32 last_addr = first_addr | (~net_mask);
-            printk(KERN_INFO "Last Address: %pI4\n", &last_addr);
-            printk(KERN_INFO "First Address: %pI4\n", &first_addr);
-            printk(KERN_INFO "Network Mask: %pI4\n", &net_mask);
-            printk(KERN_INFO "Address: %pI4\n", &addr);
-
-            printk(KERN_INFO "Blocked IP: %pI4\n", &ip_header->saddr);
+    int i = 0;
+    for (i=0;i<cidr_blocks_size; i++)
+    {
+        if (src_ip >= htonl(cidr_blocks[i].first_addr && src_ip <= cidr_blocks[i].last_addr)
+        {   
+            printk(KERN_INFO "Range: %u - %u\n", cidr_blocks[i].first_addr, cidr_blocks[i].last_addr);
+            printk(KERN_INFO "Blocked IP: %u\n", src_ip);
             return NF_DROP;
         }
     }
-
-    // printk(KERN_INFO "our netfilter is very complicated\n");
+  
     printk(KERN_INFO "Netfilter Module: Packet intercepted.\n");
     return NF_ACCEPT; // Accept the packet to continue its path
 }
+
 // Define the nf_hook_ops structure
 static struct nf_hook_ops nfho = {
     .hook = packet_hook,            // Hook function
@@ -56,6 +62,17 @@ static struct nf_hook_ops nfho = {
 };
 static int __init my_netfilter_init(void)
 {
+    int i=0;
+    for (i=0;i<cidr_blocks_size; i++)
+    {
+        __be32 addr = in_aton(cidr_blocks[i].ip_str);
+        __be32 net_mask = htonl(~((1 << (32 - cidr_blocks[i].prefix)) - 1));
+        
+        cidr_blocks[i].first_addr = (addr & net_mask);
+        cidr_blocks[i].last_addr = (cidr_blocks[i].first_addr | (~net_mask));
+        printk(KERN_INFO "Last Address: %pI4\n", &cidr_blocks[i].last_addr);
+        printk(KERN_INFO "First Address: %pI4\n", &cidr_blocks[i].first_addr);
+    }
     // Register the Netfilter hook
     nf_register_net_hook(&init_net, &nfho);
     printk(KERN_INFO "Netfilter Module: Loaded and hook registered.\n");
